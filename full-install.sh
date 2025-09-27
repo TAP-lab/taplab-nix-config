94e94ced7362d30e998d8ab0fa8d64cf6d9ebad4
clamlum <clamlum2@gmail.com>
2025-09-27 23:14:26 +1200

    first os install script iteration

---
+++ full-install.sh
@@ -0,0 +1,35 @@
+set -e
+
+parted /dev/sda -- mklabel msdos
+
+parted /dev/sda -- mkpart primary 1MB -8GB
+
+parted /dev/sda -- set 1 boot on
+
+parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+
+mkfs.ext4 -L nixos /dev/sda1
+
+mkswap -L swap /dev/sda2
+
+mount /dev/disk/by-label/nixos /mnt
+
+swapon /dev/sda2
+
+nixos-generate-config --root /mnt
+
+cd /tmp
+
+nix-env -iA nixos.git
+
+git clone https://github.com/clamlum2/taplab-nix-config.git
+
+cd taplab-nix-config
+
+rm /mnt/etc/nixos/configuration.nix
+
+cp * /mnt/etc/nixos -r
+
+nixos-install
+
+reboot
