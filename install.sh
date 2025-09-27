db009fef11cf0ff6bdc1bc791cd3ec42c7994a49
clamlum <clamlum2@gmail.com>
2025-09-27 15:00:40 +1200

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 set -e
 
+cd ~/Downloads
+
+nix-env -iA nixos.git
+
+git clone https://github.com/clamlum2/taplab-nix-config.git
+
+cd taplab-nix-config
+
 sudo rm /etc/nixos/configuration.nix
 
 sudo cp * /etc/nixos -r
