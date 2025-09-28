e036bef2db78673ec0a7811e14e979d22edb0f10
clamlum <clamlum2@gmail.com>
2025-09-28 19:41:16 +1300

    fixed profile naming

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ cd taplab-nix-config
 
 sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' --exclude='update.sh' * /etc/nixos/
 
-sudo nixos-rebuild switch --generation-name "updated from script"
+sudo nixos-rebuild switch --profile-name "updated from script"
 
 cd /tmp
 
