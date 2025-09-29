dd12c1d7eaafc3b03fd3d499296cca6f5e79c16d
clamlum <clamlum2@gmail.com>
2025-09-29 13:43:40 +1300

    changed description to comment

--- imports/prism.nix
+++ imports/prism.nix
@@ -63,6 +63,6 @@ in {
     Exec=$HOME/.local/share/PrismLauncher/offline.sh
     Icon=grass
     Categories=Game;
-    Description=Use this to play minecraft on the SurvivalLAB server.
+    Comment=Use this to play minecraft on the SurvivalLAB server.
   '';
 }
