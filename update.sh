8a3dc422f48797c4dcf408709ad5e44697e896df
clamlum <clamlum2@gmail.com>
2025-09-29 13:50:18 +1300

    minor improvments

--- update.sh
+++ update.sh
@@ -1,19 +1,27 @@
 set -e
 
+# Ensure the script is run with sudo privileges for unattended execution
 sudo echo
 
 cd /tmp
 
+# Clean up any previous update attempts
+rm -rf taplab-nix-config
+
+# Clone the latest configuration
 git clone https://github.com/clamlum2/taplab-nix-config.git
 
 cd taplab-nix-config
 
+# Sync the configuration files to /etc/nixos, excluding certain files
 sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' --exclude='update.sh' * /etc/nixos/
 
-sudo nixos-rebuild switch --profile-name "updated from script"
+# Rebuild the NixOS system 
+sudo nixos-rebuild switch --profile-name "updated from script $(date '+%Y-%m-%d_%H-%M-%S')"
 
 cd /tmp
 
+# Clean up
 rm -rf taplab-nix-config
 
 echo "Update complete, please reboot if the drivers/kernel were updated."
