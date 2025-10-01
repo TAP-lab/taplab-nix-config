95a4703d166da70c190069f4ca05cc43fb1546ce
clamt <clamlum2@gmail.com>
2025-10-01 20:31:30 +1300

    fixed command

--- update.sh
+++ update.sh
@@ -10,12 +10,12 @@ if [ -d "$CONFIG_DIR/.git" ]; then
     git pull origin "$CURRENT_BRANCH"
     sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' --exclude='update.sh' "$CONFIG_DIR/" /etc/nixos/
     sudo nixos-rebuild switch --upgrade --profile-name "config updated ($CURRENT_BRANCH) $(date '+%Y-%m-%d_%H-%M-%S')"
-    sudo nixos-collect-garbage -d
+    sudo nix-collect-garbage -d
 else
     echo "Repo not found. Cloning branch '$BRANCH' to $CONFIG_DIR..."
     git clone --branch "$BRANCH" "$REPO_URL" "$CONFIG_DIR"
     cd "$CONFIG_DIR" || { echo "Failed to cd to $CONFIG_DIR"; exit 1; }
     sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' --exclude='update.sh' "$CONFIG_DIR/" /etc/nixos/
     sudo nixos-rebuild switch --upgrade --profile-name "config updated ($BRANCH) $(date '+%Y-%m-%d_%H-%M-%S')"
-    sudo nixos-collect-garbage -d
+    sudo nix-collect-garbage -d
 fi
