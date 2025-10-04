0f4afc90db54de8026ccb5f4763461398d30c1b4
clamlum <clamlum2@gmail.com>
2025-10-04 18:54:49 +1300

    moving testing to different branch

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,6 @@ in
     pkgs.blender
     pkgs.vscode
     pkgs.luanti
-    pkgs.kitty
   ];
 
   # Enable Zsh as default shell
@@ -112,7 +111,4 @@ in
 
   # Enable Flatpak
   services.flatpak.enable = true;
-
-  # Enable OpenSSH server
-  services.openssh.enable = true;
 }
