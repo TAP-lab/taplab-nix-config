9095135152bf5ed81ec73264fed91734476438c4
clamlum <clamlum2@gmail.com>
2025-10-06 21:36:31 +1300

    updated pub key

--- imports/cache.nix
+++ imports/cache.nix
@@ -3,5 +3,5 @@
 {
   nix.settings.substituters = [ "http://192.168.1.15:5000" ];
   nix.settings.trusted-substituters = [ "http://192.168.1.15:5000" ];
-  nix.settings.trusted-public-keys = [ "local-cache:bTcHjFnTSNI0DZbDgx7sslss0784KUoQybZgUOTUbTc=" ];
+  nix.settings.trusted-public-keys = [ "local-cache:5O2Wic2bmKaczr9t6eppTnoBJ+W/4FHXEyZ0PkQG4xk=" ];
 }
