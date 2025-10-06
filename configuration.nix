5caa4c8d71e30c7312d5ae6e2aa3a3dd592bbad6
clamlum <clamlum2@gmail.com>
2025-10-06 19:15:43 +1300

    reverted to original testing branch

--- configuration.nix
+++ configuration.nix
@@ -121,8 +121,4 @@ in
 
   # Enable OpenSSH server
   services.openssh.enable = true;
-
-  nix.settings.substituters = [ "http://192.168.1.180:5000" ];
-  nix.settings.trusted-substituters = [ "http://192.168.1.180:5000" ];
-  nix.settings.trusted-public-keys = [ "local-cache:hCuO1qKsO9DuwvGqG180WyTIXPq6Gv36GWUBeux0CrA=" ];
 }
