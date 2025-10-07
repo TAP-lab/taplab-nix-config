4d031218af4aa9d32fb5a3443236077cbcbda927
clamlum <clamlum2@gmail.com>
2025-10-07 20:52:38 +1300

    updated documentation

--- imports/autoupdate.nix
+++ imports/autoupdate.nix
@@ -1,20 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  systemd.services.nixos-update = {
-    description = "Periodic NixOS system update with notification";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash /home/taplab/nix-config/imports/autoupdate.sh";
-    };
-  };
+    environment.systemPackages = with pkgs; [
+        pkgs.libnotify      # For notify-send command
+    ];
 
-  systemd.timers.nixos-update = {
-    description = "Update the system daily";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
+    # Sets up a systemd service and timer to run the autoupdate script daily
+    systemd.services.nixos-update = {
+        description = "Periodic NixOS system update with notification";
+        serviceConfig = {
+        Type = "oneshot";
+        # Executes the autoupdate script
+        ExecStart = "${pkgs.bash}/bin/bash /home/taplab/nix-config/imports/autoupdate.sh";
+        };
     };
-  };
+
+    systemd.timers.nixos-update = {
+        description = "Update the system daily";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+        OnCalendar = "daily";         # Runs the update daily
+        Persistent = true;            # Runs the job as soon as possible if it was missed
+        };
+    };
 }
--- imports/pkgs.nix
+++ imports/pkgs.nix
@@ -3,9 +3,6 @@
 {
     environment.systemPackages = with pkgs; [
         pkgs.git
-        pkgs.libnotify
-        pkgs.sudo
-        pkgs.rsync
 
         # Dependencies for the minecraft script
         pkgs.zenity
