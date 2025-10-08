11f6864031ba6f09a0681a8048d91adf16cfccc9
clamlum <clamlum2@gmail.com>
2025-10-08 18:27:34 +1300

    changed sync store alias

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -26,7 +26,11 @@
     alias nrt="sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' ~/nix-config/ /etc/nixos/ && sudo nixos-rebuild test && hyprshade on extravibrance";
     alias nrs="sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' ~/nix-config/ /etc/nixos/ && sudo nixos-rebuild switch && hyprshade on extravibrance";
     alias updatenix="sh <(curl https://raw.githubusercontent.com/clamlum2/taplab-nix-config/main/update.sh)";
-    alias updatestore="nix-copy-closure --to root@192.168.1.182 $(nix-store -qR /nix/store/*) && ssh root@192.168.1.182 'nix store sign --all --key-file /nix-serve-private --extra-experimental-features nix-command'";
+
+    function syncstore() {
+      nix-copy-closure --to root@192.168.1.182 $(nix-store -qR /nix/store/*) && \
+      ssh root@192.168.1.182 'nix store sign --all --key-file /nix-serve-private --extra-experimental-features nix-command'
+    }
 
     source ~/.oh-my-zsh/custom/themes/custom.zsh-theme
   '';
