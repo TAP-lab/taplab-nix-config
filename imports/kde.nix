49c75fd04eaec48a05ede097884a0bf17a6b8436
clamlum2 <clamlum2@gmail.com>
2025-10-09 01:14:28 +1300

    removed taskabr config

--- imports/kde.nix
+++ imports/kde.nix
@@ -27,7 +27,6 @@
     '';
 
     xdg.configFile."plasma-org.kde.plasma.desktop-appletsrc".text = ''
-        [Containments][2][Applets][5][Configuration][General]
-        launchers=file:///nix/store/zv32lw82xiwcg2kcaf465yqb80bw76bz-user-environment/share/applications/org.wezfurlong.wezterm.desktop,preferred://filemanager,file:///nix/store/5gqf746i9ga4n8xvjbcw6fp6kwycfslg-system-path/share/applications/org.kde.discover.desktop,applications:systemsettings.desktop,preferred://browser,file:///home/taplab/.local/share/applications/Minecraft.desktop,file:///nix/store/5gqf746i9ga4n8xvjbcw6fp6kwycfslg-system-path/share/applications/OrcaSlicer.desktop,file:///nix/store/5gqf746i9ga4n8xvjbcw6fp6kwycfslg-system-path/share/applications/org.inkscape.Inkscape.desktop,file:///nix/store/5gqf746i9ga4n8xvjbcw6fp6kwycfslg-system-path/share/applications/arduino-ide.desktop,file:///nix/store/5gqf746i9ga4n8xvjbcw6fp6kwycfslg-system-path/share/applications/code.desktop
+        
     '';
 }
