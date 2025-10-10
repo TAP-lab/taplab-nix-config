abd375ca32098e2111145ff21e1473f273a91222
clamt <clamlum2@gmail.com>
2025-10-10 16:09:48 +1300

    smb config

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -7,9 +7,7 @@
     device = "//nas/manuhiri";
     fsType = "cifs";
     options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts}"];
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [ "${automount_opts},guest" ];
   };
 }
