fdfbfa83672d83ca1f7fbda411081051e7524320
clamt <clamlum2@gmail.com>
2025-10-10 16:02:01 +1300

    changed mount config

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
-    fileSystems."/mnt/nas" = {
-        device = "//nas";
-        fsType = "cifs";
-        options = [ "username=guest" ];
-    };
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/nas" = {
+    device = "//nas/manuhiri";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts}"];
+  };
 }
