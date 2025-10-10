d4ebbe37e65855969437a3e54d6426b513e8b787
clamt <clamlum2@gmail.com>
2025-10-10 16:14:25 +1300

    changed options

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -6,8 +6,6 @@
   fileSystems."/mnt/nas" = {
     device = "//10.0.0.20/manuhiri";
     fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in [ "${automount_opts},guest" ];
+    options = [ "guest" ];
   };
 }
