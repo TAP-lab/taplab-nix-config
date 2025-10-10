29650604563da7d4458fb3c1033b1f4f519364aa
clamt <clamlum2@gmail.com>
2025-10-10 16:36:52 +1300

    changed nas paths

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -10,13 +10,13 @@
   };
 
   fileSystems."/mnt/nas/Hacklings" = {
-    device = "//10.0.0.20/awheawhe/STEaM/Hacklings";
+    device = "//nas/awheawhe/STEaM/Hacklings";
     fsType = "cifs";
     options = [ "guest" ];
   };
 
   fileSystems."/mnt/nas/mema" = {
-    device = "//10.0.0.20/mema";
+    device = "//nas/mema";
     fsType = "cifs";
     options = let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
