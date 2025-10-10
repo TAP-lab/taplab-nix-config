0b3fd79a8344d64683088b819e411083ee78a353
clamt <clamlum2@gmail.com>
2025-10-10 16:12:02 +1300

    changed nas ip

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -4,7 +4,7 @@
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [ pkgs.cifs-utils ];
   fileSystems."/mnt/nas" = {
-    device = "//nas/manuhiri";
+    device = "//10.0.0.20/manuhiri";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
