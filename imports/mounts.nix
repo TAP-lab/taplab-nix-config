2e9bfc6577f5b1ebfc9a85dfb4ed37a07fc1d6f6
clamt <clamlum2@gmail.com>
2025-10-10 16:27:07 +1300

    added hacklings

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -3,8 +3,14 @@
 {
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/nas" = {
-    device = "//10.0.0.20/manuhiri";
+  fileSystems."/mnt/nas/manuhiri" = {
+    device = "//nas/manuhiri";
+    fsType = "cifs";
+    options = [ "guest" ];
+  };
+
+fileSystems."/mnt/nas/Hacklings" = {
+    device = "//10.0.0.20/awheawhe/STEaM/Hacklings";
     fsType = "cifs";
     options = [ "guest" ];
   };
