83e1cebf3dad0a8e0f8c2ea7bfcff63844b731da
clamt <clamlum2@gmail.com>
2025-10-10 16:34:14 +1300

    added mema share

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -9,9 +9,17 @@
     options = [ "guest" ];
   };
 
-fileSystems."/mnt/nas/Hacklings" = {
+  fileSystems."/mnt/nas/Hacklings" = {
     device = "//10.0.0.20/awheawhe/STEaM/Hacklings";
     fsType = "cifs";
     options = [ "guest" ];
+  };
+
+  fileSystems."/mnt/nas/mema" = {
+    device = "//10.0.0.20/mema";
+    fsType = "cifs";
+    options = let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [ "${automount_opts},credentials=/etc/nixos/resources/smb-secrets" ];
   };
 }
