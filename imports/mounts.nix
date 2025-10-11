bbb153444fd4417e6d24f10e49fe3bb9aafbf140
clamlum2 <clamlum2@gmail.com>
2025-10-12 02:42:16 +1300

    removed mema auto mount for now

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -15,11 +15,11 @@
     options = [ "guest" ];
   };
 
-  fileSystems."/mnt/nas/mema" = {
-    device = "//nas/mema";
-    fsType = "cifs";
-    options = let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in [ "${automount_opts},credentials=/etc/nixos/resources/smb-secrets" ];
-  };
+  # fileSystems."/mnt/nas/mema" = {
+  #   device = "//nas/mema";
+  #   fsType = "cifs";
+  #   options = let
+  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  #   in [ "${automount_opts},credentials=/etc/nixos/resources/smb-secrets" ];
+  # };
 }
