5ee29fbe7ccf6975fd9323e9dd1d5234c134b97f
clamlum2 <clamlum2@gmail.com>
2025-10-21 00:28:08 +1300

    fixed function

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -30,8 +30,8 @@ in
     alias updatenix="sh <(curl https://raw.githubusercontent.com/clamlum2/taplab-nix-config/main/update.sh)";
 
     function syncstore() {
-      nix-copy-closure --to root@CACHE_SERVER $(nix-store -qR /nix/store/*) && \
-      ssh root@CACHE_SERVER 'nix store sign --all --key-file /nix-serve-private --extra-experimental-features nix-command'
+      nix-copy-closure --to root@$CACHE_SERVER $(nix-store -qR /nix/store/*) && \
+      ssh root@$CACHE_SERVER 'nix store sign --all --key-file /nix-serve-private --extra-experimental-features nix-command'
     }
 
     source ~/.oh-my-zsh/custom/themes/custom.zsh-theme
