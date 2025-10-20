c2edf27b644f8815e388c7e121932e94a34e1bad
clamlum2 <clamlum2@gmail.com>
2025-10-21 00:19:08 +1300

    updated cache info

--- imports/cache.nix
+++ imports/cache.nix
@@ -1,12 +1,11 @@
 { config, ... }:
 
 let
-  cacheServer = "http://192.168.1.182:5000";
-  cachePublicKey = "local-cache:lF8a6CjzBqT9J38ERYQIUcaEn/Gb5TQ/nuwq9KjTLIA="; 
+  cacheServer = "http://192.168.1.220:5000";
+  cachePublicKey = "local-cache:WrEu920wGa4xt2v2DjM0x9wf+/KLHb4+qV7tQqQJxw0="; 
 in
 
 {
   nix.settings.substituters = [ cacheServer ];
-  nix.settings.trusted-substituters = [ cacheServer ];
   nix.settings.trusted-public-keys = [ cachePublicKey ];
 }
