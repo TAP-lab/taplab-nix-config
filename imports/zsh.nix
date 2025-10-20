28e860a7d7e3087966a58ba5a75d4fb96c3b3fb6
clamlum2 <clamlum2@gmail.com>
2025-10-21 01:21:33 +1300

    fixed keys path

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -36,7 +36,7 @@ in
       sudo nix-collect-garbage -d
       sshpass -p "$SSHPASS" ssh root@$CACHE_SERVER 'nix-collect-garbage -d'
       sshpass -p "$SSHPASS" nix-copy-closure --to root@$CACHE_SERVER $(nix-store -qR /nix/store/*)
-      sshpass -p "$SSHPASS" ssh root@$CACHE_SERVER 'nix store sign --all --key-file /root/nix-serve-private --extra-experimental-features nix-command'
+      sshpass -p "$SSHPASS" ssh root@$CACHE_SERVER 'nix store sign --all --key-file /root/keys/nix-serve-private --extra-experimental-features nix-command'
     }
 
     source ~/.oh-my-zsh/custom/themes/custom.zsh-theme
