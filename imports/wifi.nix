69c21749c046adbce0631749ec650d5d3702d522
clamt <clamlum2@gmail.com>
2025-10-21 19:46:46 +1300

    added wifi

--- imports/wifi.nix
+++ imports/wifi.nix
@@ -1,5 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  wifi = builtins.tryEval (import /etc/nixos/fetch/wifi-details.nix);
+in
 {
-    
+  networking.wireless.enable = true;
+  networking.wireless.networks =
+    if wifi.success then {
+      "${wifi.value.ssid}" = {
+        psk = wifi.value.psk;
+      };
+    } else {};
 }
