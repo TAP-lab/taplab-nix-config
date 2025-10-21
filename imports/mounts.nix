2e94493b7b381bfbbdc6ec1671ba49dacb700d20
clamt <clamlum2@gmail.com>
2025-10-21 20:18:00 +1300

    enabled mounts

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -1,22 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  # # Mounts the nas drive
-  # environment.systemPackages = [ pkgs.cifs-utils ];
-  # fileSystems."/mnt/nas/manuhiri" = {
-  #   device = "//nas/manuhiri";
-  #   fsType = "cifs";
-  #   options = [ "nofail" "noauto" "guest" ];
-  # };
+  # Mounts the nas drive
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/nas/manuhiri" = {
+    device = "//nas/manuhiri";
+    fsType = "cifs";
+    options = [ "nofail" "noauto" "guest" ];
+  };
 
-  # # Mounts the Hacklings share
-  # fileSystems."/mnt/nas/Hacklings" = {
-  #   device = "//nas/awheawhe/STEaM/Hacklings";
-  #   fsType = "cifs";
-  #   options = [ "nofail" "noauto" "guest" ];
-  # };
+  # Mounts the Hacklings share
+  fileSystems."/mnt/nas/Hacklings" = {
+    device = "//nas/awheawhe/STEaM/Hacklings";
+    fsType = "cifs";
+    options = [ "nofail" "noauto" "guest" ];
+  };
 
-  # # Mounts the mema share with automount options (disabled for now until credentials system is in place)
+  # Mounts the mema share with automount options (disabled for now until credentials system is in place)
   # fileSystems."/mnt/nas/mema" = {
   #   device = "//nas/mema";
   #   fsType = "cifs";
