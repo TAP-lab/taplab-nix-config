3d6a4399ae786d2143cc6a0ec7c94d6ea298a034
clamt <clamlum2@gmail.com>
2025-10-22 13:58:34 +1300

    changed options

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -19,8 +19,6 @@
   fileSystems."/mnt/test" = {
     device = "//192.168.1.220/media";
     fsType = "cifs";
-     options = let
-         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-     in [ "${automount_opts},credentials=/etc/nixos/secrets/mema" "nofail" ];
+     options = [ "credentials=/etc/nixos/secrets/mema" "nofail" "noauto" ];
   };
 }
