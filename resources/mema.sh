2c7832c3e893f6040359531b598c264481aa6969
clamt <clamlum2@gmail.com>
2025-10-22 14:04:32 +1300

    udpaed mema command

--- resources/mema.sh
+++ resources/mema.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo mkdir -p /etc/nixos/secrets
+
 URL="http://192.168.1.220:8080/mema"
 
-curl -fsSL "$URL" -o /etc/nixos/secrets/mema
+sudo curl -fsSL "$URL" -o /etc/nixos/secrets/mema
