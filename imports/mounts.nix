63c3398b8068c89319aea989ef3ae7d0c4718794
clamt <clamlum2@gmail.com>
2025-10-22 14:07:15 +1300

    changed mount options

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -19,6 +19,6 @@
   fileSystems."/mnt/nas/mema" = {
     device = "//192.168.1.220/media";
     fsType = "cifs";
-     options = [ "credentials=/etc/nixos/secrets/mema" "nofail" "noauto" ];
+     options = [ "credentials=/etc/nixos/secrets/mema" "nofail" ];
   };
 }
