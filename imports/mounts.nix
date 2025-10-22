420cf4bfda01aba72a05a7a42241450a2fb02b85
clamt <clamlum2@gmail.com>
2025-10-22 13:42:13 +1300

    added nofail

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -30,6 +30,6 @@
     fsType = "cifs";
      options = let
          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-     in [ "${automount_opts},credentials=/etc/nixos/code/test-login" ];
+     in [ "${automount_opts},credentials=/etc/nixos/code/test-login" "nofail" ];
   };
 }
