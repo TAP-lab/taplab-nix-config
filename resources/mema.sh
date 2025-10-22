90ca476942ed80c77c204eddff844064e9ad6e3c
clamt <clamlum2@gmail.com>
2025-10-22 14:49:24 +1300

    updated mema script

--- resources/mema.sh
+++ resources/mema.sh
@@ -5,3 +5,5 @@ sudo mkdir -p /etc/nixos/secrets
 URL="http://192.168.1.220:8080/mema"
 
 sudo curl -fsSL "$URL" -o /etc/nixos/secrets/mema
+
+sudo mount -t cifs //192.168.1.220/mema /mnt/nas/mema -o credentials=/etc/nixos/secrets/mema,uid=1000,gid=100,file_mode=0644,dir_mode=0755
