48410a5d249e98b3f06fafab38d66f80cb8c1636
clamt <clamlum2@gmail.com>
2025-10-22 13:15:35 +1300

    added test mount

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -24,4 +24,10 @@
   #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
   #   in [ "${automount_opts},credentials=/etc/nixos/resources/smb-secrets" ];
   # };
+
+  fileSystems."/mnt/test" = {
+    device = "//192.168.1.209/media";
+    fsType = "cifs";
+    options = [ "nofail" "noauto" "guest" ];
+  }
 }
