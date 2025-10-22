287071052ea610d0e83691e7e6222049ecf38f79
clamt <clamlum2@gmail.com>
2025-10-22 13:55:25 +1300

    disabled share

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -15,21 +15,4 @@
     fsType = "cifs";
     options = [ "nofail" "noauto" "guest" ];
   };
-
-  # Mounts the mema share with automount options (disabled for now until credentials system is in place)
-  # fileSystems."/mnt/nas/mema" = {
-  #   device = "//nas/mema";
-  #   fsType = "cifs";
-  #   options = let
-  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-  #   in [ "${automount_opts},credentials=/etc/nixos/resources/smb-secrets" ];
-  # };
-
-  fileSystems."/mnt/test" = {
-    device = "//192.168.1.220/media";
-    fsType = "cifs";
-     options = let
-         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-     in [ "${automount_opts},credentials=/etc/nixos/code/test-login" "nofail" ];
-  };
 }
--- resources/mema.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-URL="http://192.168.1.220:8080/mema"
-
-curl -fsSL "$URL" -o /etc/nixos/secrets/mema
