2348d7c3a98c119135d34c29f34bfa5fec6b145c
clamt <clamlum2@gmail.com>
2025-10-22 13:57:26 +1300

    added share

--- imports/mounts.nix
+++ imports/mounts.nix
@@ -15,4 +15,12 @@
     fsType = "cifs";
     options = [ "nofail" "noauto" "guest" ];
   };
+
+  fileSystems."/mnt/test" = {
+    device = "//192.168.1.220/media";
+    fsType = "cifs";
+     options = let
+         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+     in [ "${automount_opts},credentials=/etc/nixos/secrets/mema" "nofail" ];
+  };
 }
