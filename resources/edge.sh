d314fc6c1ce09f0a3d9f2b0920d79df9b5b133ba
clamt <clamlum2@gmail.com>
2025-10-23 15:10:46 +1300

    script changes

--- resources/edge.sh
+++ resources/edge.sh
@@ -3,17 +3,22 @@
 set -e
 
 pkill msedge
+echo "Closed Microsoft Edge."
 
 cd ~/.config/microsoft-edge
+echo "Changed directory to Microsoft Edge config."
 
 curl -fsSL http://192.168.1.220:8080/edge -o Default.tar.xz
+echo "Downloaded new profile archive."
 
 rm -rf Default
+echo "Removed old Default profile."
 
 tar -xf Default.tar.xz 
+echo "Extracted new Default profile."
 
 rm Default.tar.xz
+echo "Removed profile archive."
 
 microsoft-edge & 
-
-exit
+echo "Launched Microsoft Edge with new profile."
