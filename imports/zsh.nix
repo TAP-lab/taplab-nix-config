7ff17ba1f0e8b67ed6009eb473609a187b284cf3
clamt <clamlum2@gmail.com>
2025-10-24 14:34:07 +1300

    enabled aliases

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -22,8 +22,8 @@
     alias nrt="sudo rsync -av --exclude='.git' ~/nix-config/ /etc/nixos/ && sudo nixos-rebuild test";
     alias nrs="sudo rsync -av --exclude='.git' ~/nix-config/ /etc/nixos/ && sudo nixos-rebuild switch";
     alias updatenix="sh <(curl https://raw.githubusercontent.com/clamlum2/taplab-nix-config/main/update.sh)";
-    # alias wifi="bash /etc/nixos/resources/wifi.sh";
-    # alias mema="bash /etc/nixos/resources/mema.sh";
+    alias wifi="bash /etc/nixos/resources/wifi.sh";
+    alias mema="bash /etc/nixos/resources/mema.sh";
     alias edge="bash /etc/nixos/resources/edge.sh";
 
     source ~/.oh-my-zsh/custom/themes/custom.zsh-theme
