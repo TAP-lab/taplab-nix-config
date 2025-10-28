4ab9963450b8ccc717e6b22d8e4891a2f507e6bc
clamt <clamlum2@gmail.com>
2025-10-28 16:02:04 +1300

    removed mema mount as automount works fine

--- scripts/mema.sh
+++ scripts/mema.sh
@@ -18,14 +18,3 @@ fi
 
 # Makes the credentials file readable only by root
 sudo chmod 600 /etc/nixos/secrets/mema
-
-# # Mounts the share on the fly (usually returns an error but still works)
-# echo "Mounting CIFS share..."
-# echo "This returns an error but still works, just check the file explorer to confirm."
-# echo
-# if sudo mount -t cifs //nas/mema /mnt/nas/mema -o credentials=/etc/nixos/secrets/mema,uid=1000,gid=100,file_mode=0644,dir_mode=0755; then
-#     echo "CIFS share mounted successfully."
-# else
-#     echo "Failed to mount CIFS share." >&2
-#     exit 1
-# fi
