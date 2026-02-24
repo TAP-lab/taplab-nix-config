0a7d1447b483d6e49d4b98326070503e1420d59c
clamlum2 <clamlum2@gmail.com>
2026-02-24 20:39:02 +1300

    tweaking orcaslicer config

--- modules/apps/orcaslicer.nix
+++ modules/apps/orcaslicer.nix
@@ -2,8 +2,8 @@
 
 {
   home.activation.copyOrcaSlicerConfig = ''
-    mkdir -p ${config.home.homeDirectory}/.config/orcaslicer
-    ${pkgs.gnutar}/bin/tar -xJf ${./../../resources/OrcaSlicer/OrcaSlicer.tar.xz} \
-      -C ${config.home.homeDirectory}/.config/orcaslicer
+    mkdir -p ${config.home.homeDirectory}/.config/OrcaSlicer
+    ${pkgs.gnutar}/bin/tar -xJf ${../../resources/OrcaSlicer/OrcaSlicer.tar.xz} \
+      -C ${config.home.homeDirectory}/.config/OrcaSlicer
   '';
 }
