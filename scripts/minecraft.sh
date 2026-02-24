85902220b50e5ddcc9a26a35b3570c89db78f98c
clamlum2 <clamlum2@gmail.com>
2026-02-24 13:58:13 +1300

    updated minecraft script

--- scripts/minecraft.sh
+++ scripts/minecraft.sh
@@ -21,20 +21,6 @@ fi
 # Replaces CHANGETHISNAME with the inputted name in accounts.json
 sed -i "s/CHANGETHISNAME/$input_name/g" accounts.json
 
-# Kills any running instance of prism launcher otherwise the game does not launch
-pkill prismlauncher
-
 # Launches game and automatically connects to the TAPLab server
 prismlauncher -l taplab -a $input_name -s 10.0.0.181 &
 
-# Waits for the Prism window to appear and then closes it
-while true; do                                          # Loop indefinitely
-    win_id=$(kdotool getactivewindow)                   # Gets the ID of the currently active window
-    win_name=$(kdotool getwindowname "$win_id")         # Gets the name of the window with that ID
-    if [[ "$win_name" == *Prism* ]]; then               # Checks if the window name contains "Prism"
-        # If true, it closes the window using a KDE global shortcut
-        qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut "Window Close"
-        break       # Exit the loop after closing the window
-    fi
-    sleep 0.1       # Loops the check every 0.1 seconds (10x per second)
-done
