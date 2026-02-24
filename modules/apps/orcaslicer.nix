bf26e787c16ce6d9e8fbff7bea07452354d9895e
clamlum2 <clamlum2@gmail.com>
2026-02-24 20:42:29 +1300

    fixed orcaslicer config extract dir

--- modules/apps/orcaslicer.nix
+++ modules/apps/orcaslicer.nix
@@ -2,8 +2,7 @@
 
 {
   home.activation.copyOrcaSlicerConfig = ''
-    mkdir -p ${config.home.homeDirectory}/.config/OrcaSlicer
     ${pkgs.gnutar}/bin/tar -xf ${../../resources/OrcaSlicer/OrcaSlicer.tar.xz} \
-      -C ${config.home.homeDirectory}/.config/OrcaSlicer
+      -C ${config.home.homeDirectory}/.config/
   '';
 }
