916a5e74faf581eeac3bc6488832dc48b2701ec4
clamlum2 <clamlum2@gmail.com>
2026-03-13 15:44:02 +1300

    rewrote gb studio package definition

--- modules/apps/gb-studio.nix
+++ modules/apps/gb-studio.nix
@@ -1,40 +1,14 @@
-{ pkgs ? import <nixpkgs> {}, icon }:
+# This defines a custom nix package for GB Studio, and sets it up with an appimage wrapper.
+{ fetchurl, appimageTools, stdenv }:
 
-pkgs.stdenv.mkDerivation {
+let
   pname = "gb-studio";
-  version = "4.1.3";
-  src = pkgs.fetchurl {
-    url = "https://github.com/chrismaltby/gb-studio/releases/download/v4.1.3/gb-studio-linux.AppImage";
-    sha256 = "45c2e83d852571bd8e143ddf990766cc383a2854e3a9a527dffc80b9089b99ed";
-  };
-  unpackPhase = "true";
-  buildInputs = [ pkgs.appimage-run ];
-  installPhase = ''
-    mkdir -p $out/bin $out/share/appimage $out/share/applications $out/share/pixmaps
-    cp $src $out/share/appimage/gb-studio-linux.AppImage
-    cat > $out/bin/gb-studio <<EOF
-    #!${pkgs.stdenv.shell}
-    exec ${pkgs.appimage-run}/bin/appimage-run $out/share/appimage/gb-studio-linux.AppImage "\$@"
-    EOF
-    chmod +x $out/bin/gb-studio
+  version = "4.2.2";
 
-    cat > $out/share/applications/gb-studio.desktop <<EOF
-    [Desktop Entry]
-    Version=1.0
-    Type=Application
-    Name=GB Studio
-    Exec=$out/bin/gb-studio
-    Icon=gb-studio
-    Terminal=false
-    Categories=Development;Game;
-    EOF
-
-    cp ${icon} $out/share/pixmaps/gb-studio.png
-  '';
-  meta = with pkgs.lib; {
-    description = "GB Studio (AppImage wrapper)";
-    homepage = "https://github.com/chrismaltby/gb-studio";
-    license = licenses.mit;
-    platforms = platforms.linux;
+  src = fetchurl {
+    url = "https://github.com/chrismaltby/gb-studio/releases/download/v${version}/gb-studio-linux.AppImage";
+    sha256 = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
   };
-}
+in
+
+appimageTools.wrapType2 { inherit pname version src; }
