229d31e104e5db6af6d7cddaa0d98ed4677cb07b
clamlum2 <clamlum2@gmail.com>
2026-03-27 09:54:16 +1300

    added ssh key to root for testing

--- modules/debug.nix
+++ modules/debug.nix
@@ -7,6 +7,8 @@
 
   # My SSH public key for passwordless login.
   users.users.taplab.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/sXIx+I7BCq6T4QfiEWqvh+E1d9+y4CrTijURf5Wsq clamt" ];
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/sXIx+I7BCq6T4QfiEWqvh+E1d9+y4CrTijURf5Wsq clamt" ];
+
 
   # Needed to run the VSCode SSH server.
   programs.nix-ld.enable = true;
