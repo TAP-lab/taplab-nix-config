7a2e0fde7c976dee0d9e1409f169a8152ab0019d
clamlum2 <clamlum2@gmail.com>
2026-05-13 16:39:20 +1200

    cleaned up flake

--- flake.nix
+++ flake.nix
@@ -17,84 +17,90 @@
     };
   };
 
-  outputs = inputs@ {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    nixpkgs-old,
-    home-manager,
-    plasma-manager,
-    ...
-  }:
-  {
-    # Defines the main system configuration for the laptops
-    nixosConfigurations = {
-      nixos = let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-      in nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          # Imports the nixos modules
-          ./hardware-configuration.nix
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      nixpkgs-old,
+      home-manager,
+      plasma-manager,
+      ...
+    }:
+    {
+      # Defines the main system configuration for the laptops
+      nixosConfigurations = {
+        nixos =
+          let
+            system = "x86_64-linux";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              # Imports the nixos modules
+              ./hardware-configuration.nix
 
-          ./modules/configuration.nix
-          ./modules/pkgs.nix
-          ./modules/nas.nix
-          ./modules/auto-update.nix
+              ./modules/configuration.nix
+              ./modules/pkgs.nix
+              ./modules/nas.nix
+              ./modules/auto-update.nix
 
-          ./modules/debug.nix
+              ./modules/debug.nix
 
-          ./modules/credentials.nix
+              ./modules/credentials.nix
 
-          ./modules/desktop/kde.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                nixpkgs-unstable = nixpkgs-unstable;
-                nixpkgs-old = nixpkgs-old;
-                self = self;
-              };
-              sharedModules = [ plasma-manager.homeModules.plasma-manager ];
-              users = {
-                taplab = { pkgs, ... }: {
-                  imports = [
-                    # Imports the home manager modules
-                    ./modules/home.nix
+              ./modules/desktop/kde.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  backupFileExtension = "backup";
+                  extraSpecialArgs = {
+                    nixpkgs-unstable = nixpkgs-unstable;
+                    nixpkgs-old = nixpkgs-old;
+                    self = self;
+                  };
+                  sharedModules = [ plasma-manager.homeModules.plasma-manager ];
+                  users = {
+                    taplab =
+                      { ... }:
+                      {
+                        imports = [
+                          # Imports the home manager modules
+                          ./modules/home.nix
 
-                    ./modules/shell/zsh.nix
-                    ./modules/shell/themes/taplab-theme.nix
+                          ./modules/shell/zsh.nix
+                          ./modules/shell/themes/taplab-theme.nix
 
-                    ./modules/desktop/plasma-manager.nix
+                          ./modules/desktop/plasma-manager.nix
 
-                    ./modules/apps/wezterm.nix
-                    ./modules/apps/minecraft.nix
-                    ./modules/apps/orcaslicer.nix
-                  ];
+                          ./modules/apps/wezterm.nix
+                          ./modules/apps/minecraft.nix
+                          ./modules/apps/orcaslicer.nix
+                        ];
+                      };
+                    root =
+                      { ... }:
+                      {
+                        home.username = "root";
+                        home.homeDirectory = "/root";
+                        home.stateVersion = "25.11";
+                        imports = [
+                          ./modules/shell/zsh.nix
+                          ./modules/shell/themes/taplab-theme.nix
+                        ];
+                      };
+                  };
                 };
-                root = { pkgs, ... }: {
-                  home.username = "root";
-                  home.homeDirectory = "/root";
-                  home.stateVersion = "25.11";
-                  imports = [
-                    ./modules/shell/zsh.nix
-                    ./modules/shell/themes/taplab-theme.nix
-                  ];
-                };
-              };
+              }
+            ];
+            specialArgs = {
+              inherit nixpkgs-unstable;
+              inherit nixpkgs-old;
+              inherit self;
             };
-          }
-        ];
-        specialArgs = {
-          inherit nixpkgs-unstable;
-          inherit nixpkgs-old;
-          inherit self;
-        };
+          };
       };
     };
-  };
 }
