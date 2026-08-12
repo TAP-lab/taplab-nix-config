7f2a6b0910d42e1cf9f57169b59d8cd6513d47f9
clamlum2 <clamlum2@gmail.com>
2026-08-12 19:28:20 +1200

    update gb studio to  4.3.2

--- modules/apps/gb-studio.nix
+++ modules/apps/gb-studio.nix
@@ -1,13 +1,13 @@
 # This defines a custom nix package for GB Studio, and sets it up with an appimage wrapper.
-{ fetchurl, appimageTools, stdenv }:
+{ fetchurl, appimageTools }:
 
 let
   pname = "gb-studio";
-  version = "4.2.2";
+  version = "4.3.2";
 
   src = fetchurl {
     url = "https://github.com/chrismaltby/gb-studio/releases/download/v${version}/gb-studio-linux.AppImage";
-    sha256 = "sha256-GscN9nFyDqgTEgx9bQI78blDJOlynkZX83cgqLIHkQ8=";
+    sha256 = "sha256-EvI+h4l0+/y5esaQExVnpCxQ8mfQkKOeLDmOiLhpWOA=";
   };
 in
 
