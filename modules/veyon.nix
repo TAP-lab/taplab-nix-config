c8f7eafc423328f9ae206d3b331d4b03b8816476
ajasnz <alex@ajas.nz>
2026-09-03 16:07:15 +1200

    Veyon public keys

--- modules/veyon.nix
+++ modules/veyon.nix
@@ -4,6 +4,10 @@
 
   services.veyon = {
     enable = true;
+    publicKey = {
+      name = "alex";
+      value = builtins.readFile ../resources/veyon/alex.pub;
+    };
   };
 
   # The upstream module only exposes a single named key via
@@ -12,7 +16,6 @@
   # (one per admin/user) are installed the same way here. Add one entry
   # per keypair, named after who it belongs to.
   environment.etc = {
-    "veyon/keys/public/alex/key".source = ../resources/veyon/alex.pub;
     "veyon/keys/public/devops/key".source = ../resources/veyon/devops.pub;
     "veyon/keys/public/holly/key".source = ../resources/veyon/holly.pub;
     "veyon/keys/public/minecraft/key".source = ../resources/veyon/minecraft.pub;
